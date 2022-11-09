d3a81e5de7c5f0c4c875346511f9484907b2f8bd
SukhetskyiS <medpredin@gmail.com>
2022-11-09 21:20:18 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { changeFilter } from '../redux/filterSlice.js';
-import { getFilter } from 'redux/selectors';
+import { getFilter } from '../redux/selectors';
 
 export function Filter() {
   const filter = useSelector(getFilter);
